f86ab80efe2890bb4b8b40cc4ba573624d693855
JaxxtotheMaxx <offbeat5024@gmail.com>
2020-04-06 17:50:08 -0400

    Added routes for get and post

--- app.js
+++ app.js
@@ -13,12 +13,30 @@ app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended: true}));
 
 app.listen(5000, () => {
-  MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+  MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if(error) {
       throw error;
     }
     database = client.db(DATABASE_NAME);
     collection = database.collection("personnel");
     console.log("Connected to `" + DATABASE_NAME + "`!");
-  })
+  });
+});
+
+app.post("/personnel", (request, response) => {
+  collection.insertOne(request.body, (error, result) => {
+    if(error) {
+      return response.status(500).send(error);
+    }
+    response.send(result.result)
+  });
+});
+
+app.get("/personnel", (request, response) => {
+  collection.find({}).toArray((error, result) => {
+    if(error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
+  });
 });
