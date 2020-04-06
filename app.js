7272960f7a68ffc26baf0528587c17943371edc5
JaxxtotheMaxx <offbeat5024@gmail.com>
2020-04-06 16:42:21 -0400

    Set MongoClient to connect to database, added Environment variables for connection url and database name

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+require('dotenv').config();
 const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const DATABASE_NAME = process.env.DATABASE_NAME;
 
-require('dotenv').config();
 
 let app = Express();
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended: true}));
 
-app.listen(5000, () => {});
+app.listen(5000, () => {
+  MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+      throw error;
+    }
+    database = client.db(DATABASE_NAME);
+    collection = database.collection("personnel");
+    console.log("Connected to `" + DATABASE_NAME + "`!");
+  })
+});
